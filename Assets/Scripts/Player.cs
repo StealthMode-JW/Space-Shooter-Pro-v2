81aaa010a42e295fe9c77130387f3573e4f53bb6
StealthMode-JW <64043636+StealthMode-JW@users.noreply.github.com>
2020-04-28 10:00:27 -0700

    first two changes of Framework assignment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d71239a3bf69e4f2e2bf29d231f6108569ba32555f56e8409b5aec6ec0faeb2
-size 6136
+oid sha256:2c9dcbfd239f9522c57078023e5be4f4afa55b24307b05bdf3771245ab13a280
+size 8692
